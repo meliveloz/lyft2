3a8b5b2b46da6cc397de597f30262401a7a34464
meliveloz <mvelozcampos@gmail.com>
2017-12-15 22:51:41 -0300

    comment

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){ //Función para la vista splash
 	$(function(){
    	setTimeout(function() {
       $('#splash').fadeOut(1000);
@@ -6,7 +6,7 @@ $(document).ready(function(){
   });
   
  
-	var randomNumber = (function() {
+	var randomNumber = (function() {  // Para la generar un número de 3 dígitos random
  	var code = "";
   	var str = "1234567890";
  	 for (var i = 0; i < 3; i++) {
@@ -15,18 +15,18 @@ $(document).ready(function(){
   return code;
 });
 	
-    $("#input-key").keyup(function() {
+    $("#input-key").keyup(function() {   //validando que solo sean validos los números 
       var notWord = /^[0-9]*$/;
       var inputValue = $(this).val();
-      if (inputValue.length >= 10 && notWord.test(inputValue)) {
-        $("#btn-next").removeClass("disabled");
+      if (inputValue.length >= 10 && notWord.test(inputValue)) { //para que no ingresen menos de 10 números y se cumpla que solo ingresan letras.
+        $("#btn-next").removeClass("disabled");// habilitar botones si las condiciones se dan.
       } else {
         $("#btn-next").addClass("disabled");
       }
     });
 
 
-    $("#btn-next").click(function() {
+    $("#btn-next").click(function() {  //enviando código con alarma
        alert("Tu código es "+randomNumber());
      
       });
@@ -35,8 +35,8 @@ $(document).ready(function(){
       alert("Tu código es "+ randomNumber());
 
     });
-
-    $("#verify-key").keyup(function() {
+//_________________________________________________________validando los input_________________________________________________
+    $("#verify-key").keyup(function() { 
       var notWord = /^[0-9]*$/;
       var inputValue = $(this).val();
       if (inputValue.length == 3 && notWord.test(inputValue)) {
